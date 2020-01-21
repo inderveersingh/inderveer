a8d0d6c29f682cea51c09805e40ab75bf3552f20
Inderveer Singh <748465@student.tor.loc>
2020-01-21 11:00:06 -0500

    part A section E

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace c0748465
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("state your number:");
+            double UserInput = Convert.ToDouble(Console.ReadLine());
+            Multiply m = new Multiply(UserInput);
+        }
+    }
+    class Multiply
+    {
+        public Multiply(double UserInput)
+        {
+            Console.WriteLine("you are in constructer!");
+            for (int i=1;i<=10;i++)
+            {
+                Console.WriteLine("{0}times{1}={2}", UserInput, i, UserInput * i);
+
+            }
         }
     }
 }
